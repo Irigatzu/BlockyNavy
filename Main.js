3a0556046bc55e22dd876b60043b0b4278a4ed6c
irigatzue <irigoyenasier@opendeusto.es>
2021-03-15 11:54:08 +0100

    Cannon block

--- Main.js
+++ Main.js
@@ -1,7 +1,8 @@
-
+<script src="https://unpkg.com/blockly/blockly.min.js"></script>
 <script src="blockly_compressed.js"></script>
 <script src="blocks_compressed.js"></script>
 <script src="msg/js/en.js"></script>
+import Blockly from 'blockly';
 <div id="blocklyDiv" style="height: 480px; width: 600px;"></div>
 <xml id="toolbox" style="display: none">
   <block type="controls_if"></block>
