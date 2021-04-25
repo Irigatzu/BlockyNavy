67547f2ec495ddd211ffcaa19c070669e396a5ff
irigatzue <irigoyenasier@opendeusto.es>
2021-04-25 19:26:51 +0200

    reorder

--- src/game.js
+++ src/game.js
@@ -7,7 +7,7 @@ ctx.fillRect(0, 0, 600, 600);
 const GAME_WIDTH = 600;
 const GAME_HEIGTH = 600;
 
-let player = new Player(300, 200);
+let player = new Player(200, 200);
 
 let lastTime = 0;
 
@@ -17,7 +17,7 @@ function gameLoop(timestamp) {
   ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGTH);
   player.update(deltaTime);
   player.draw(ctx);
-  player.move(15, 0);
+  player.move(0, 0);
   requestAnimationFrame(gameLoop);
 }
 gameLoop();
